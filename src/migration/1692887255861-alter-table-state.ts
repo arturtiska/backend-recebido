2533bc7e4540346c1fccafb667ce95a7ac356d90
arturtiska <artur.reezende@hotmail.com>
2023-08-24 15:26:04 -0300

    feat: created module controller and service for search city and state

--- src/migration/1692887255861-alter-table-state.ts
+++ src/migration/1692887255861-alter-table-state.ts
@@ -3,9 +3,18 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class AlterTableState1692887255861 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+        ALTER TABLE state
+            ADD uf varchar(2) NOT NULL
+        `)
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+    ALTER TABLE state
+        drop uf;
+        `)
     }
 
 }
